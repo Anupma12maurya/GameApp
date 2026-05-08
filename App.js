9e1056dd50c012a6357f1fb2e61a24a058f2eb5e
Anupma Maurya <anupamamaurya10@gmail.com>
2026-05-08 11:32:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './screens/Home';
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+import Dashboard from './screens/Dashboard';
+import Leaderboard from './screens/Leaderboard';
+
+export default function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/leaderboard" element={<Leaderboard />} />
+            </Routes>
+        </Router>
+    );
+}
